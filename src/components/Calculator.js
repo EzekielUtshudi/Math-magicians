03c43f06db2ef6541506d40b2e715b6551e486d6
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-04-19 15:13:17 +0200

    the event and use of stat and props

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Calculator.css';
+import PropTypes from 'prop-types';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -8,35 +9,41 @@ class Calculator extends React.Component {
   }
 
   render() {
+    const { updateDetails, value } = this.props;
     return (
       <div className="container">
         <div className="screen">
-          0
+          {value || 0}
         </div>
         <div className="buttons">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="orange" type="button">&divide;</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="orange" type="button">&times;</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="orange" type="button">&minus;</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="orange" type="button">+</button>
-          <button className="double" type="button">0</button>
-          <button type="button">.</button>
-          <button className="orange" type="button">=</button>
+          <button onClick={updateDetails} type="button">AC</button>
+          <button onClick={updateDetails} type="button">+/-</button>
+          <button onClick={updateDetails} type="button">%</button>
+          <button onClick={updateDetails} className="orange" type="button">&divide;</button>
+          <button onClick={updateDetails} type="button">7</button>
+          <button onClick={updateDetails} type="button">8</button>
+          <button onClick={updateDetails} type="button">9</button>
+          <button onClick={updateDetails} className="orange" type="button">&times;</button>
+          <button onClick={updateDetails} type="button">4</button>
+          <button onClick={updateDetails} type="button">5</button>
+          <button onClick={updateDetails} type="button">6</button>
+          <button onClick={updateDetails} className="orange" type="button">&minus;</button>
+          <button onClick={updateDetails} type="button">1</button>
+          <button onClick={updateDetails} type="button">2</button>
+          <button onClick={updateDetails} type="button">3</button>
+          <button onClick={updateDetails} className="orange" type="button">+</button>
+          <button onClick={updateDetails} className="double" type="button">0</button>
+          <button onClick={updateDetails} type="button">.</button>
+          <button onClick={updateDetails} className="orange" type="button">=</button>
         </div>
       </div>
     );
   }
 }
+
+Calculator.propTypes = {
+  updateDetails: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Calculator;
