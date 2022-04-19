5da509a909e231972edd0d7a4eaa5fd8f4859c91
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-04-19 20:18:25 +0200

    refactor with hooks

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -111,8 +111,6 @@ export default (obj, buttonName) => {
     };
   }
 
-  // no operation yet, but the user typed one
-
   // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
     return { operation: buttonName };
